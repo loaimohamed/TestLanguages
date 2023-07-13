7fd222bfb1ddbc55d5753a10b94801ca7da10aaf
Loai Mohamed <39069035+loaimohamed@users.noreply.github.com>
2023-07-13 19:49:40 +0000

    2

---
+++ rust_test/target/debug/deps/rust_test-ce40d9d7fcec61f4.d
@@ -0,0 +1,5 @@
+/workspaces/TestingG/rust_test/target/debug/deps/rust_test-ce40d9d7fcec61f4.rmeta: src/main.rs
+
+/workspaces/TestingG/rust_test/target/debug/deps/rust_test-ce40d9d7fcec61f4.d: src/main.rs
+
+src/main.rs:
---
+++ rust_test/target/debug/rust_test.d
@@ -0,0 +1,1 @@
+/workspaces/TestingG/rust_test/target/debug/rust_test: /workspaces/TestingG/rust_test/src/main.rs
